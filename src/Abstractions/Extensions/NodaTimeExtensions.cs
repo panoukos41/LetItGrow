0acd94fd86497050d596f2c9cf261ced87b0c42c
Panos Athanasiou <panos_ath@outlook.com>
2021-01-15 23:09:19 +0200

    Refactor

---
+++ src/Abstractions/Extensions/NodaTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NodaTime
+{
+    public static class NodaTimeExtensions
+    {
+        public static DateTime ToLocalTime(this Instant instant)
+        {
+            return instant.ToDateTimeUtc().ToLocalTime();
+        }
+    }
+}
